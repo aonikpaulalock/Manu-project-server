d8a95f8703962a3544bff10b255fe7d2a90ede1d
aonikpaulalock <alockpaul9845@gmail.com>
2022-09-08 09:16:07 +0600

    aded blogs api

--- index.js
+++ index.js
@@ -22,15 +22,99 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function run() {
   try {
     client.connect()
-    const inventoryCollection = client.db("manufacture").collection("tools");
+    const toolsCollection = client.db("manufacture").collection("tools");
+    const orderCollection = client.db("manufacture").collection("orders");
+    const blogsCollection = client.db("manufacture").collection("blogs");
+    
 
-    // Load  Inventory
+    // Tools Collection
     app.get("/tools", async (req, res) => {
-      const query = {};
-      const cursor = inventoryCollection.find(query);
+      const result = await toolsCollection.find().toArray();
+      res.send(result)
+    });
+
+    // Specifice Id Tools
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const tools = await toolsCollection.findOne(filter);
+      res.send(tools);
+    });
+
+    // Add Users
+    app.post("/tools", async (req, res) => {
+      const tools = req.body;
+      const result = await toolsCollection.insertOne(tools)
+      res.send(result)
+    });
+
+    app.delete("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await toolsCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+
+ // Order- Data
+ 
+    // User Order Data
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      // const query = { email: order.email }
+      const result = await orderCollection.insertOne(order);
+      res.send(result)
+    });
+
+    // User Ordering-Data per User
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email }
+      const cursor = orderCollection.find(filter);
       const result = await cursor.toArray()
       res.send(result);
-    })
+    });
+
+    // Load All User Aded Order
+    app.get("/orders", async (req, res) => {
+      const result = await orderCollection.find().toArray()
+      res.send(result)
+    });
+
+    // Order Data Delete Normal User
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // Payment Order Data
+    app.get("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await orderCollection.findOne(filter);
+      res.send(result);
+    });
+
+
+
+    
+    // Load All Blogs
+    app.get("/blogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const blogs = await blogsCollection.findOne(filter);
+      res.send(blogs);
+    });
+
+
+
 
   }
   catch {
